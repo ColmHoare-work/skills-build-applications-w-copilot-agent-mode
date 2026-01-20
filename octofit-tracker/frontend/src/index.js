1733386217819eb0c810a45247def2e30fc7d1f1
ColmHoare-work <138237803+ColmHoare-work@users.noreply.github.com>
2026-01-20 11:08:43 +0000

    Rest Frontend - Attempt 2

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// App.js now handles BrowserRouter and routing
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
